3f0790d102a3ffb8e31f3605993669f5c4508d60
AlexKaikin <lex.kaikin@gmail.com>
2024-02-18 20:19:40 +0400

    perf: added profile settings to club

--- src/app/club/_elements/Notes/Notes.tsx
+++ src/app/club/_elements/Notes/Notes.tsx
@@ -6,7 +6,7 @@ import dayjs from 'dayjs'
 import Image from 'next/image'
 import defaultAvatar from '@assets/img/user/defaultAvatar.png'
 import { useAuth, useNotes } from '@store'
-import { IconButton } from '@ui'
+import { IconButton, Typography } from '@ui'
 import styles from './Notes.module.css'
 
 export function Notes() {
@@ -45,27 +45,30 @@ export function Notes() {
     <div className={styles.notes}>
       {notes.map(({ user, _id, text, created }) => (
         <div key={_id} className={styles.note}>
+          <div className={styles.card}>
+            <div className={styles.avatar}>
+              <Image
+                fill
+                sizes="(max-width: 1800px) 33vw"
+                src={user.avatarUrl ? user.avatarUrl : defaultAvatar}
+                alt="avatar"
+              />
+            </div>
+            <div className={styles.content}>
+              <div>
+                {text.split('\n').map((item, i) => (
+                  <p key={i}>{item}</p>
+                ))}
+              </div>
+              <div className={styles.time}>
+                <Typography variant="tooltip">
+                  {dayjs(created).format('H:m, DD.MM.YYYY')}
+                </Typography>
+              </div>
+            </div>
+          </div>
           <div className={styles.control}>
             <IconButton icon="BsTrash3" onClick={() => deleteNote(_id)} />
-          </div>
-          <div className={styles.avatar}>
-            <Image
-              fill
-              sizes="(max-width: 1800px) 33vw"
-              src={user.avatarUrl ? user.avatarUrl : defaultAvatar}
-              alt="avatar"
-            />
-          </div>
-          <div className={styles.content}>
-            <div className={styles.name}>{user!.fullName}</div>
-            <div className="note__text">
-              {text.split('\n').map((item, i) => (
-                <p key={i}>{item}</p>
-              ))}
-            </div>
-            <div className={styles.time}>
-              {dayjs(created).format('H:m, DD.MM.YYYY')}
-            </div>
           </div>
         </div>
       ))}
