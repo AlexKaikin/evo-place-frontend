a29f103ab5c18965611fd193f53b01aaf3b30839
Alex <lex.kaikin@gmail.com>
2024-05-12 14:00:56 +0400

    feat: Edit product review form

--- src/ui/Form/Form.tsx
+++ src/ui/Form/Form.tsx
@@ -8,34 +8,23 @@ import {
   SubmitHandler,
   UseFormReturn,
 } from 'react-hook-form'
-import cn from 'classnames'
-import styles from './Form.module.css'
 
 type Props<Values extends FieldValues = FieldValues> = {
   formMethods: UseFormReturn<Values>
   onSubmit: SubmitHandler<Values>
   onSubmitError?: SubmitErrorHandler<Values>
-  variant?: 'column' | 'row'
 } & Omit<FormHTMLAttributes<HTMLFormElement>, 'onSubmit'>
 
 export function Form<Values extends FieldValues = FieldValues>(
   props: PropsWithChildren<Props<Values>>
 ) {
-  const {
-    variant = 'column',
-    children,
-    formMethods,
-    onSubmit,
-    onSubmitError,
-    ...rest
-  } = props
+  const { children, formMethods, onSubmit, onSubmitError, ...rest } = props
   return (
     <FormProvider {...formMethods}>
       <form
         noValidate
         {...rest}
         onSubmit={formMethods.handleSubmit(onSubmit, onSubmitError)}
-        className={cn(styles.form, styles[variant])}
       >
         {children}
       </form>
